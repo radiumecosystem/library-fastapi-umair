0275d7592133934097df0a41e34a51e4965fa892
Umairdvg <mailtowebotics@gmail.com>
2022-10-26 15:38:55 +0530

    config

--- config/db.py
+++ config/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://u118790815_fastapi:Fastapi123@217.21.91.1/u118790815_fastapi")
+engine = create_engine("mysql+pymysql://u118790815_fastapi:Fastapi123@sql787.main-hosting.eu/u118790815_fastapi")
 meta = MetaData()
 conn = engine.connect()
